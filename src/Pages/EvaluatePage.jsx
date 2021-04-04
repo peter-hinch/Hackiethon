2b2cb0e486c95c9a2a2fab436ef7ddeeb26cb2bf
Troy <tulyatt@student.holmesglen.edu.au>
2021-04-04 15:38:55 +1000

    changed evaluate page, uncommented code

--- src/Pages/EvaluatePage.jsx
+++ src/Pages/EvaluatePage.jsx
@@ -3,24 +3,19 @@ import Evaluator from '../Components/EvaluatePage/Evaluator';
 import {getStrings} from '../Components/EvaluatePage/resultStrings';
 
 class EvaluatePage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = this.props;
-    }
+    
     state = {
-        // strings: getStrings(),
+        strings: getStrings(),
 
-        // satisfaction: 20,
-        // stress: 60,
-        // physical: 100,
+        satRes: 20,
+        strRes: 60,
+        phyRes: 100,
 
-        // autonomy: "yes",
-        // connections: "sometimes",
-        // growth: "no",
+        autRes: "yes",
+        conRes: "sometimes",
+        grwRes: "no",
 
         // oneThingToChange: "placeholder",
-
-        // calledStrings: [],
     };
 
     callResultByInt(index, variable) {
@@ -63,32 +58,32 @@ class EvaluatePage extends Component {
                     <Evaluator
                         title={this.state.strings[this.callTitle(0)]}
                         description={this.state.strings[this.callDescription(0)]}
-                        result={this.state.strings[this.callResultByInt(0, this.state.satisfaction)]}
+                        result={this.state.strings[this.callResultByInt(0, this.state.satRes)]}
                     />
                     <Evaluator 
                         title={this.state.strings[this.callTitle(1)]}
                         description={this.state.strings[this.callDescription(1)]}
-                        result={this.state.strings[this.callResultByInt(1, this.state.stress)]}
+                        result={this.state.strings[this.callResultByInt(1, this.state.strRes)]}
                     />
                     <Evaluator 
                         title={this.state.strings[this.callTitle(2)]}
                         description={this.state.strings[this.callDescription(2)]}
-                        result={this.state.strings[this.callResultByInt(2, this.state.physical)]}
+                        result={this.state.strings[this.callResultByInt(2, this.state.phyRes)]}
                     />
                     <Evaluator 
                         title={this.state.strings[this.callTitle(3)]}
                         description={this.state.strings[this.callDescription(3)]}
-                        result={this.state.strings[this.callResultByString(3, this.state.autonomy)]}
+                        result={this.state.strings[this.callResultByString(3, this.state.autRes)]}
                     />
                     <Evaluator
                         title={this.state.strings[this.callTitle(4)]}
                         description={this.state.strings[this.callDescription(4)]}
-                        result={this.state.strings[this.callResultByString(4, this.state.connections)]}
+                        result={this.state.strings[this.callResultByString(4, this.state.conRes)]}
                     />
                     <Evaluator 
                         title={this.state.strings[this.callTitle(5)]}
                         description={this.state.strings[this.callDescription(5)]}
-                        result={this.state.strings[this.callResultByString(5, this.state.growth)]}
+                        result={this.state.strings[this.callResultByString(5, this.state.grwRes)]}
                     />
                 </div>
             </React.Fragment>
